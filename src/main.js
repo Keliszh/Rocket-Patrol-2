a982fdb77a2c72bb019874f602a7e26bc46a841c
X Christopher Z <xzhan281@ucsc.edu>
2021-06-30 22:30:50 +0800

    add  submission informations under the main.js

--- src/main.js
+++ src/main.js
@@ -15,8 +15,10 @@ let borderPadding = borderUISize / 2;
 let keyF, keyR, keyLEFT, keyRIGHT, keyH, keyLEFT02, keyRIGHT02;
 
 
-/*------Possible attempt in this project------
-(I am listing all that I think I should have done already, but I maybe misunderstood some rules, or I have done something I ddin't even notice.)
+/*------Xingwei Christopher Zhang---------
+-------Rocket Patrol: Demon Slayer--------
+--------------06 - 29 - 2021--------------
+-----Possible Attempt in this Project-----
 1. Tutorial
 2. Display the time remaining in sec
 3. Replace the UI with new artwork
@@ -25,7 +27,13 @@ let keyF, keyR, keyLEFT, keyRIGHT, keyH, keyLEFT02, keyRIGHT02;
 6. Create a new spaceship type with new artwork that is smaller faster and worth more points
 7. Create new artwork for all of the in-game assets(Rocket, spaceships, explosion)
 8. Create and implement a new weapon
-9. Implement a simultaneous two-player mode.
+9. Implement a simultaneous two-player mode
 10.Redesign the game's artwork, UI, and Sound to change it theme and aesthetic other than sci-fi
 ----------------------------------------------
+-I Want to Thank for People who Helped Me on the Ideas of Rocket Patro and Git Settings-
+1. Isaac Karth
+2. Guanchen Liu
+3. Chenyang Xu
+4. Yongbo Wang
+They are My Teachers and My Teammates of CMPM120 2021 Summer.
 */
